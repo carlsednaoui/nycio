903e15e8bc2ff99f147b5396d798ceb2bebc346e
miclovich <vicmiclovich@gmail.com>
2014-04-17 20:30:18 -0400

    Add migration and defaults

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417235929) do
+ActiveRecord::Schema.define(version: 20140418002703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140417235929) do
     t.string   "name"
     t.boolean  "public",                 default: false
     t.date     "birthdate"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
