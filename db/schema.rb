e2a1e3938622b61fe7d49bd3b120b613f2b7a315
Matthew Star <mattjstar@gmail.com>
2014-04-18 11:05:56 -0400

    add occurrence model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140418145514) do
+ActiveRecord::Schema.define(version: 20140418150513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20140418145514) do
   create_table "events", force: true do |t|
     t.string   "name"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "occurrences", force: true do |t|
+    t.integer  "event_id"
+    t.datetime "time"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
