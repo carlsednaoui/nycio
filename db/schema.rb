125772eac849cbe1834b74d2671912f48fbb127c
miclovich <vicmiclovich@gmail.com>
2014-04-19 15:12:53 -0400

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140418162416) do
+ActiveRecord::Schema.define(version: 20140419191139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20140418162416) do
     t.boolean  "public",                 default: false
     t.date     "birthdate"
     t.boolean  "admin",                  default: false
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
