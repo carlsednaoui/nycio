51076b37fb54f0504809f03d45ead6b8f70f4bcb
miclovich <vicmiclovich@gmail.com>
2014-04-21 11:59:57 -0400

    test passing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419193047) do
+ActiveRecord::Schema.define(version: 20140421155908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140419193047) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "skills", force: true do |t|
