3dc11c389346a3de8fbbd0f54d6185ad35aa0eb4
Matthew Star <mattjstar@gmail.com>
2014-05-01 23:18:15 -0400

    add index table to admin_organizations view

--- app/controllers/admin/admin_pages_controller.rb
+++ app/controllers/admin/admin_pages_controller.rb
@@ -1,12 +1,15 @@
 module Admin
 	class AdminPagesController < ApplicationController
 		def organizations
+			@organizations = Organization.all
 		end
 
 		def events
+			@events = Event.all
 		end
 
 		def users
+			@users = User.all
 		end
 	end
 end
