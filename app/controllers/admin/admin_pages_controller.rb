c808deae64e70055cdd57417d6274157c980b0c9
Matthew Star <mattjstar@gmail.com>
2014-05-01 23:29:24 -0400

    users must be logged in to access admin_pages

--- app/controllers/admin/admin_pages_controller.rb
+++ app/controllers/admin/admin_pages_controller.rb
@@ -1,5 +1,7 @@
 module Admin
 	class AdminPagesController < ApplicationController
+		before_action :authenticate_user!
+		
 		def organizations
 			@organizations = Organization.all
 		end
---
+++ spec/views/admin/admin_pages/organizations.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "admin/admin_pages/organizations.html.erb" do
+
+end
