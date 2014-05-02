c7b064eeb6801f8f6ae361715eff6a4f6bcafdf6
Matthew Star <mattjstar@gmail.com>
2014-05-01 22:38:30 -0400

    add test data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Admin Users
+
 user1 = User.create(email: "carl@thinkful.com",
 			password: "nyciopassword")
 user1.add_role "admin"
@@ -17,3 +19,40 @@ user2.add_role "admin"
 user3 = User.create(email: "mstar@thinkful.com",
 			password: "nyciopassword")
 user3.add_role "admin"
+
+
+# Organizations
+
+org = Organization.create(name: "Test Organization",
+						  description: "What a mighty fine test organization.")
+
+
+# Organization Manager
+
+user1.add_role "manager", org
+
+
+# Event
+
+event = Event.create(name: "Test Event",
+					 description: "GONNA TEACH SOME KIDS TO CODE at this lovely event!!!",
+					 organization: org)
+
+
+# Skills
+
+skill = Skill.create(name: "HTML")
+skill2 = Skill.create(name: "Ruby on Rails")
+
+
+# Event Skills
+
+EventSkill.create(skill: skill, event: event)
+EventSkill.create(skill: skill2, event: event)
+
+
+# Occurrence
+
+Occurrence.create(event: event,
+				  start_time: (DateTime.now + 30.days),
+				  end_time: (DateTime.now + 30.days + 3.hours))
