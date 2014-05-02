aefed5902785f72950ed590a9eac2c979221c7bd
Matthew Star <mattjstar@gmail.com>
2014-05-01 21:27:04 -0400

    add end_time to occurrences

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501204153) do
+ActiveRecord::Schema.define(version: 20140502012553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140501204153) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "start_time"
+    t.datetime "end_time"
   end
 
   create_table "organizations", force: true do |t|
