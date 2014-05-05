9ca15385efe6833c66abd91419982ab1c6568f3a
Matthew Star <mattjstar@gmail.com>
2014-05-05 18:19:21 -0400

    hide design routes, re-enable home root route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,18 @@
 class PagesController < ApplicationController
-  def events
+  def home
     @events = Event.all
     @skills = Skill.all
   end
 
-  def show_event
-    @event = Event.find(params[:id])
-  end
+  # def show_event
+  #   @event = Event.find(params[:id])
+  # end
 
-  def organizations
-    @organizations = Organization.all
-  end
+  # def organizations
+  #   @organizations = Organization.all
+  # end
 
-  def show_organization
-    @organization = Organization.find(params[:id])
-  end
+  # def show_organization
+  #   @organization = Organization.find(params[:id])
+  # end
 end
