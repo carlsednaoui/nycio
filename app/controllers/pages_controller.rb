011cb7414a0b2c3a4fd3ea8cdeae4bd2eb80cee9
Matthew Star <mattjstar@gmail.com>
2014-05-05 18:27:33 -0400

    remove design actions in pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,4 @@ class PagesController < ApplicationController
     @events = Event.all
     @skills = Skill.all
   end
-
-  # def show_event
-  #   @event = Event.find(params[:id])
-  # end
-
-  # def organizations
-  #   @organizations = Organization.all
-  # end
-
-  # def show_organization
-  #   @organization = Organization.find(params[:id])
-  # end
 end
