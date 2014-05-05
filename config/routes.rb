fdbfb380fa88d378baf9f058328e1c59a905169e
Matthew Star <mattjstar@gmail.com>
2014-05-05 18:19:21 -0400

    hide design routes, re-enable home root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-  root 'pages#events'
-  get '/organizations', to: 'pages#organizations'
-  get '/events/:id', to: 'pages#show_event', as: 'event'
-  get '/organizations/:id', to: 'pages#show_organization', as: 'organization'
+  root 'pages#home'
 
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
+  resources :organizations
 
 	namespace :admin do
 		get '/organizations', to: 'admin_pages#organizations'
 		get '/events', to: 'admin_pages#events'
 		get '/users', to: 'admin_pages#users'
 	end
-
-  resources :organizations
 end
--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,32 +2,4 @@ require 'spec_helper'
 
 describe OrganizationsController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'edit'" do
-    it "returns http success" do
-      get 'edit'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
 end
