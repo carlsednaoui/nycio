ca01b71429ec6f5176d2c188c35652bf64cca0cc
Matthew Star <mattjstar@gmail.com>
2014-05-06 17:24:10 -0400

    split User name attribute to first_name and last_name, remove User admin attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140502012553) do
+ActiveRecord::Schema.define(version: 20140506211739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,13 +92,13 @@ ActiveRecord::Schema.define(version: 20140502012553) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
     t.boolean  "public",                 default: false
     t.date     "birthdate"
-    t.boolean  "admin",                  default: false
     t.string   "phone"
     t.string   "gender"
     t.boolean  "active",                 default: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
