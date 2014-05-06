25c1496e76aac06d38fce7b72154367746690a6f
Matthew Star <mattjstar@gmail.com>
2014-05-05 20:10:53 -0400

    add upcoming_occurrence() method to admin_pages_helper

--- app/helpers/admin_pages_helper.rb
+++ app/helpers/admin_pages_helper.rb
@@ -1,2 +1,12 @@
 module AdminPagesHelper
+	def upcoming_occurrence(event)
+		if event.occurrences.present?
+			occurrence = event.occurrences.reorder('start_time ASC').first
+			start_time = occurrence.start_time.strftime("%b %d, %Y %l:%M %p")
+			end_time = occurrence.end_time.strftime("%b %d, %Y %l:%M %p")
+			"#{start_time} - #{end_time}"
+		else
+			"None"
+		end
+	end
 end
