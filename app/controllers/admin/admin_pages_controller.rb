36041f42e62397247e5c2031240dc647ee86840e
Matthew Star <mattjstar@gmail.com>
2014-05-05 23:00:34 -0400

    add sortable name column to organiadmin_organizations table

--- app/controllers/admin/admin_pages_controller.rb
+++ app/controllers/admin/admin_pages_controller.rb
@@ -2,9 +2,11 @@ module Admin
 	class AdminPagesController < ApplicationController
 		before_action :authenticate_user!
 		before_action :run_authorize
+		helper_method :sort_column, :sort_direction
 		
 		def organizations
-			@organizations = Organization.all
+			organizations = Organization.all
+			@organizations = organizations.order(sort_column + " " + sort_direction)
 		end
 
 		def events
@@ -19,6 +21,14 @@ module Admin
 
 		def run_authorize
 			authorize! :manage, :all
+		end
+
+		def sort_column
+			Organization.column_names.include?(params[:sort]) ? params[:sort] : "organizations.updated_at"
+		end
+
+		def sort_direction
+			%w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
 		end
 	end
 end
