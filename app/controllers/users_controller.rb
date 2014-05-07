6b54beddee9c89df09a5ecfb104bc7b8a21cb6b7
Matthew Star <mattjstar@gmail.com>
2014-05-06 20:01:49 -0400

    enable edit User functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,26 @@ class UsersController < ApplicationController
 	end
 
 	def update
+		@user = User.find(params[:id])
+
+		respond_to do |format|
+			if @user.update_attributes(user_params)
+				format.html { redirect_to @user, notice: 'Account Updated Successfully' }
+			else
+				format.html { render action: 'edit' }
+			end
+		end
 	end
 
 	def destroy
+	end
+
+	private
+
+
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation,
+									 :first_name, :last_name, :birthdate,
+									 :gender, :phone)
 	end
 end
