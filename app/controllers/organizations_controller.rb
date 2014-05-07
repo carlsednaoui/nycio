8f298dce535734bad968d95a3e0c2f21f6ff929f
Matthew Star <mattjstar@gmail.com>
2014-05-07 13:35:25 -0400

    enable new and create Organizations

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,16 +1,26 @@
 class OrganizationsController < ApplicationController
   def index
-  	@organizations = Organization.all
+    @organizations = Organization.all
   end
 
   def show
-  	@organization = Organization.find(params[:id])
+    @organization = Organization.find(params[:id])
   end
 
   def new
+    @organization = Organization.new
   end
 
   def create
+    @organization = Organization.new(organization_params)
+
+    respond_to do |format|
+      if @organization.save
+        format.html { redirect_to root_path, notice: 'New Organization proposed' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
   end
 
   def edit
@@ -20,5 +30,11 @@ class OrganizationsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def organization_params
+    params.require(:organization).permit(:name, :description)
   end
 end
