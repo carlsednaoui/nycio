c1e2116ba5d7308801cb665bed8931b199324183
Matthew Star <mattjstar@gmail.com>
2014-05-07 12:21:48 -0400

    add crud actions to organizations controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,13 +2,22 @@ class OrganizationsController < ApplicationController
   def index
   end
 
-  def edit
-  end
-
   def show
   	@organization = Organization.find(params[:id])
   end
 
   def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
