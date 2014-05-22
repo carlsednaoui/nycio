4e894ee39a2a0c80126588b289b01d6a5b66c116
miclovich <vicmiclovich@gmail.com>
2014-05-22 16:02:26 +0300

    column to track deletion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507215744) do
+ActiveRecord::Schema.define(version: 20140522125544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20140507215744) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.boolean  "approved",    default: false
+    t.datetime "deleted_at"
   end
 
   create_table "roles", force: true do |t|
