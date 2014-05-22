bc86368fc7c3c106f77b15d353168f657131ffb0
miclovich <vicmiclovich@gmail.com>
2014-05-22 16:32:34 +0300

    add a couple of helper methods and valiations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,18 @@
 class User < ActiveRecord::Base
   rolify
+
+  before_save :set_volunteer_role
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  ROLES = %w[admin]
+  ROLES = %w[admin volunteer]
 
   has_many :user_organizations, dependent: :destroy
   has_many :organizations, through: :user_organizations
+
+  validates :first_name, :last_name, :phone, :gender, :birthdate, presence: true
 
   has_many :user_skills, dependent: :destroy
   has_many :skills, through: :user_skills
@@ -24,7 +29,25 @@ class User < ActiveRecord::Base
     return name.join(' ')
   end
 
+  def is_public?
+    self.public
+  end
+
+  def is_volunteer?
+    self.roles.where(name: 'volunteer').exists?
+  end
+
+  def is_admin?
+    self.roles.where(name: 'admin').exists?
+  end
+
+
   def manager_organizations
     self.organizations.where('user_organizations.manager IS true')
   end
+
+  private
+    def set_volunteer_role
+      self.add_role :volunteer
+    end
 end
