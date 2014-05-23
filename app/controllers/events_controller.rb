8061e3cdd922a57421274ce2677e03436aec60cd
Matthew Star <mattjstar@gmail.com>
2014-05-23 11:46:27 -0400

    create new event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,13 +2,14 @@ class EventsController < ApplicationController
   def index
   end
 
-  def edit
-  end
-
   def show
   	@event = Event.find(params[:id])
   end
 
   def new
+  	@event = Event.new
+  end
+
+  def edit
   end
 end
