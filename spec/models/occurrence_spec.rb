b1f79f88e98ce448871b2c3dda6d5d20f8aadce6
Matthew Star <mattjstar@gmail.com>
2014-05-23 16:43:06 -0400

    add occurrence validations

--- spec/models/occurrence_spec.rb
+++ spec/models/occurrence_spec.rb
@@ -8,4 +8,8 @@ describe Occurrence do
 	it { should respond_to(:start_time) }
 
 	it { should belong_to(:event) }
+
+	it { should validate_presence_of :event_id }
+	it { should validate_presence_of :start_time }
+	it { should validate_presence_of :end_time }
 end
