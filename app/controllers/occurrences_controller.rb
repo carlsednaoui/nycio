9310462a389988e7785a03d4a113c4a86781f038
Matthew Star <mattjstar@gmail.com>
2014-05-23 16:20:47 -0400

    add new and create occurrence logic

--- app/controllers/occurrences_controller.rb
+++ app/controllers/occurrences_controller.rb
@@ -1,13 +1,29 @@
 class OccurrencesController < ApplicationController
 	def new
+		@occurrence = Occurrence.new
 	end
 
 	def create
+		@occurrence = Occurrence.new(occurrence_params)
+
+		respond_to do |format|
+			if @occurrence.save
+				format.html { redirect_to root_path, notice: 'New Occurrence added' }
+			else
+				format.html { render action: 'new' }
+			end
+		end
 	end
 
 	def edit
 	end
 
 	def update
+	end
+
+	private
+
+	def occurrence_params
+		params.require(:event).permit(:start_time, :end_time, :event_id)
 	end
 end
