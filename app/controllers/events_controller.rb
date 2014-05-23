ea2e05289f1f367cdc1729bdb105bf767a57629c
Matthew Star <mattjstar@gmail.com>
2014-05-23 12:24:54 -0400

    alphabetize organization select box for new/edit events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,7 +12,7 @@ class EventsController < ApplicationController
 
   def new
   	@event = Event.new
-  	@user_orgs = current_user.is_admin? ? Organization.all : current_user.manager_organizations
+  	@user_orgs = current_user.is_admin? ? Organization.order(:name) : current_user.manager_organizations
   end
 
   def create
