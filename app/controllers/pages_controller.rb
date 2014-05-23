84e47ac03b189bb062cdcce98dc8563915ad88b7
Matthew Star <mattjstar@gmail.com>
2014-05-23 12:10:05 -0400

    only show events on the home page with occurrences

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @events = Event.all
+    @events = Event.includes(:occurrences).where("occurrences.event_id IS NOT NULL").references(:occurrences)
     @skills = Skill.all
   end
 end
