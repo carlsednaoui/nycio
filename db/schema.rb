4fd215740dccbf182596edd5e1359bbf4f8bff79
carl sednaoui <carlsednaoui@gmail.com>
2014-05-24 14:53:07 -0400

    Add website to organization

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524183707) do
+ActiveRecord::Schema.define(version: 20140524185052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20140524183707) do
     t.string   "contact_name"
     t.string   "contact_email"
     t.string   "contact_phone"
+    t.string   "website"
   end
 
   create_table "roles", force: true do |t|
