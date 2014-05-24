15ee85d0cd24cdc18d807f585d02e5de27e43dac
Matthew Star <mattjstar@gmail.com>
2014-05-23 21:36:29 -0400

    add sortability by skill to home page events

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,4 +14,12 @@ class Event < ActiveRecord::Base
 	def self.with_future_occurrences
 		includes(:occurrences).where("occurrences.start_time > ?", Time.now).references(:occurrences)
 	end
+
+	def self.with_skills(skill_ids)
+		if skill_ids
+			includes(:event_skills).where("event_skills.skill_id in (?)", skill_ids).references(:event_skills)
+		else
+			all
+		end
+	end
 end
