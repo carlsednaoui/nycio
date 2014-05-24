a864688d1dec151294f7547e032a828a5504bf8d
carl sednaoui <carlsednaoui@gmail.com>
2014-05-24 15:02:18 -0400

    Allow organizations to add their own image url for logo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524185052) do
+ActiveRecord::Schema.define(version: 20140524185936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20140524185052) do
     t.string   "contact_email"
     t.string   "contact_phone"
     t.string   "website"
+    t.string   "image_url"
   end
 
   create_table "roles", force: true do |t|
