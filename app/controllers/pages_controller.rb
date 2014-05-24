ff16f6850cd6b4585d9ac1fa80608414b94f9bf2
Matthew Star <mattjstar@gmail.com>
2014-05-23 21:36:29 -0400

    add sortability by skill to home page events

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @events = Event.from_approved_orgs.with_future_occurrences
+    @events = Event.from_approved_orgs.with_future_occurrences.with_skills(params[:skill_id])
     @skills = Skill.all
   end
 end
