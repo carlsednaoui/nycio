266133a387c3c1c37d090732a78dce4f38fb85d2
Matthew Star <mattjstar@gmail.com>
2014-05-23 21:11:11 -0400

    only show events on the home page with occurrences in the future

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @events = Event.from_approved_orgs.with_occurrences
+    @events = Event.from_approved_orgs.with_future_occurrences
     @skills = Skill.all
   end
 end
