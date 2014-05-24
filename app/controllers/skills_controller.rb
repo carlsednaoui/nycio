744264347537ae43cef27b84533187948969ee9a
Matthew Star <mattjstar@gmail.com>
2014-05-23 21:49:02 -0400

    add skill validations, enable skill create action

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -4,5 +4,20 @@ class SkillsController < ApplicationController
 	end
 
 	def create
+		@skill = Skill.new(skill_params)
+
+		respond_to do |format|
+			if @skill.save
+				format.html { redirect_to admin_skills_path, notice: 'New Skill added' }
+			else
+				format.html { render action: 'new' }
+			end
+		end
+	end
+
+	private
+
+	def skill_params 
+		params.require(:skill).permit(:name)
 	end
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -8,4 +8,7 @@ describe Skill do
 
 	it { should have_many(:users) }
 	it { should have_many(:events) }
+
+	it { should validate_presence_of :name }
+	it { should validate_uniqueness_of :name }
 end
