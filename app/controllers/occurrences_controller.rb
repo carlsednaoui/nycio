01554dcae86e1ad9294da5189c3efb3f0bc49135
carl sednaoui <carlsednaoui@gmail.com>
2014-05-24 21:46:04 -0400

    Fix the way managers can create events and occurrences. Close #50 & #51

--- app/controllers/occurrences_controller.rb
+++ app/controllers/occurrences_controller.rb
@@ -11,7 +11,7 @@ class OccurrencesController < ApplicationController
 
 		respond_to do |format|
 			if @occurrence.save
-				format.html { redirect_to root_path, notice: 'New Occurrence added' }
+				format.html { redirect_to events_path, notice: 'New Occurrence added' }
 			else
 				format.html { render action: 'new' }
 				@user_events = current_user.is_admin? ? Event.order(:name) : current_user.manager_events
