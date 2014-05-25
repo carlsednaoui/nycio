518522ad5e4638e4df7c91aefcbb6eca808edce1
carl sednaoui <carlsednaoui@gmail.com>
2014-05-24 21:54:35 -0400

    Make help_build field accessible

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class ApplicationController < ActionController::Base
     														:last_name,
     														:birthdate,
                                 :phone,
-                                :gender) }
+                                :gender,
+                                :help_build) }
   end
 end
